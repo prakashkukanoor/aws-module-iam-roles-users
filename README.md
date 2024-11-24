d312141fc0b6e503eb52a1accf2dc0c9d03c0fbf
prakashkukanoor <prakashkop054@gmail.com>
2024-11-24 13:33:27 -0500

    initial setup

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# aws-module-iam-roles-users
-Create IAM policy, roles and users
+# aws-module-iam
+Module to create IAM user, user group
+
+# Generate public and private keys using gpg
+- Install Homebrew (Mac os)
+[Home Brew](https://formulae.brew.sh/formula/gnupg)
+
+- List existing keys
+```
+gpg --list-secret-keys
+```
+
+- Generate new keys
+```
+gpg --full-generate-key
+gpg --armor --export <email>@gmail.com > public-key.asc
+gpg --armor --export-secret-keys <key-from-above-command> > private-key.asc
+```
+
+- Decrypt the content
+```
+echo "-----BEGIN PGP MESSAGE----- 
+<encrypted content>
+-----END PGP MESSAGE-----" | gpg --decrypt
+```
+
+
+
+
+
---
+++ main.tf
@@ -0,0 +1,100 @@
+locals {
+  policies = jsondecode(templatefile("${var.path_to_json_file}", {
+    dynamodb_table_name = var.dynamodb_table_name
+    bucket_name         = var.bucket_name
+  }))
+  path_to_group = { for policy in local.policies : policy.path => policy.name }
+  common_tags = {
+    environment = var.environment
+    owner       = var.team
+    createdBy   = "terraform"
+  }
+
+}
+
+resource "aws_iam_policy" "this" {
+  for_each = { for policy in local.policies : policy.name => policy }
+
+  name        = "${each.value.name}-policy"
+  path        = each.value.path
+  description = "iam policy"
+
+  policy = jsonencode(each.value.policy_statement)
+  tags   = local.common_tags
+}
+
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "this" {
+  name               = "ec2-assume-role"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy.arn
+}
+
+
+
+resource "aws_iam_group" "this" {
+  for_each = aws_iam_policy.this
+
+  name = "${each.key}-group"
+}
+
+resource "aws_iam_group_policy_attachment" "this" {
+  for_each = aws_iam_policy.this
+
+  group      = aws_iam_group.this[each.key].name
+  policy_arn = each.value.arn
+}
+
+resource "aws_iam_user" "users" {
+  for_each = { for users in var.users : users.user => users.path }
+
+  name = each.key
+  path = each.value
+
+  tags = local.common_tags
+}
+
+resource "aws_iam_access_key" "this" {
+  for_each = aws_iam_user.users
+  pgp_key  = file("${var.public_key_file_path}")
+
+  user = each.value.name
+}
+
+resource "aws_iam_user_group_membership" "user_group_membership" {
+  for_each = aws_iam_user.users
+
+  user = each.value.name
+  groups = [
+    aws_iam_group.this[local.path_to_group[each.value.path]].name
+  ]
+}
+
+resource "local_file" "csv_file" {
+  filename = "iam_users_access_keys.csv"
+
+  content = <<EOT
+UserName,AccessKeyId,SecretAccessKey
+${join("\n", [for user, access_key in aws_iam_access_key.this : "${access_key.user},${access_key.id},${access_key.encrypted_secret}"])}
+EOT
+}
+
+
+
+
